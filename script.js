5ecb3bfa977f538b43ef7c0223cd37e27610c1cd
Maiara <gutierremaiara@gmail.com>
2019-10-01 21:52:18 -0300

    inicio slider

---
+++ script.js
@@ -0,0 +1,16 @@
+setInterval(function(){
+	var	slideAtivo = document.querySelector('.slide.ativo');
+  var proximo = slideAtivo.nextElementSibling;
+
+
+if(proximo == null){
+	proximo = document.querySelector('.slide');
+}
+ slideAtivo.classList.remove('ativo');
+ proximo.classList.add('ativo')
+
+}, 3000);
+
+
+console.log('ATIVO', slideAtivo);
+console.log('PROXIMO', proximo);
